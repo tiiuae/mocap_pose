d828d38fb7407422c2cdbcb1b3db1b0296be7491
Sergey Smirnov <sergey.smirnov@unikie.com>
2021-06-01 14:46:34 +0300

    Final cosmetic changes before "merging to master"

--- src/mocap_pose_node.cpp
+++ src/mocap_pose_node.cpp
@@ -1,12 +1,12 @@
+/// @file mopcap_pose_node.cpp : ROS2 node running Mocap -> PX4 GPS convertor
+
 #include "mocap_pose/mocap_pose.hpp"
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     std::shared_ptr<MocapPose> node = std::make_shared<MocapPose>();
-    //std::thread w_thread(indoor_pos_worker, node);
     rclcpp::spin(node);
-    //w_thread.join();
     rclcpp::shutdown();
     return 0;
 }
